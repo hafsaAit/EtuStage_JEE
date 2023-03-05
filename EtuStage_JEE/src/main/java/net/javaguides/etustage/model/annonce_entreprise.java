38cee4b10855fe2a287b98e6c54ec1cf9eb0737e
Hafsa AIT EL ATIK <hafsaaitelatik@gmail.com>
2023-03-05 14:59:41 +0000

    AnnEntp_update

--- EtuStage_JEE/src/main/java/net/javaguides/etustage/model/annonce_entreprise.java
+++ EtuStage_JEE/src/main/java/net/javaguides/etustage/model/annonce_entreprise.java
@@ -1,102 +1,119 @@
 package net.javaguides.etustage.model;
 
-public class annonce_entreprise {
+
 	
-	private int Id_AnnEntrp ;
-	private String Titre ;
-	private String Description_Annc;
-	private String Duree;
-	private String Type_Stag;
-	private int Id_Entrp;
-	
-	
-	public annonce_entreprise(int id_AnnEntrp, String titre, String description_Annc,  String duree,
-			String type_Stag, int id_Entrp) {
-		super();
-		Id_AnnEntrp = id_AnnEntrp;
-		Titre = titre;
-		Description_Annc = description_Annc;
+	public class annonce_entreprise {
+
+		private int Id_AnnEntrp;
+		private String Titre;
+		private String Domaine;
 		
-		Duree = duree;
-		Type_Stag = type_Stag;
-		Id_Entrp = id_Entrp;
-	}
+
+		private String Description_Annc;
+		private String Duree;
+		private String Type_Stag;
+		private int Id_Entrp;
+		
+		
+		public annonce_entreprise(int id_AnnEntrp, String titre, String domaine, String description_Annc, String duree,
+				String type_Stag, int id_Entrp) {
+			super();
+			Id_AnnEntrp = id_AnnEntrp;
+			Titre = titre;
+			Domaine = domaine;
+			Description_Annc = description_Annc;
+			Duree = duree;
+			Type_Stag = type_Stag;
+			Id_Entrp = id_Entrp;
+		}
 
 
-	public annonce_entreprise(String titre, String description_Annc,  String duree, String type_Stag,
-			int id_Entrp) {
-		super();
-		Titre = titre;
-		Description_Annc = description_Annc;
-	
-		Duree = duree;
-		Type_Stag = type_Stag;
-		Id_Entrp = id_Entrp;
-	}
+		public annonce_entreprise(String titre, String domaine, String description_Annc, String duree, String type_Stag,
+				int id_Entrp) {
+			super();
+			Titre = titre;
+			Domaine = domaine;
+			Description_Annc = description_Annc;
+			Duree = duree;
+			Type_Stag = type_Stag;
+			Id_Entrp = id_Entrp;
+		}
 
 
-	public int getId_AnnEntrp() {
-		return Id_AnnEntrp;
-	}
+		public int getId_AnnEntrp() {
+			return Id_AnnEntrp;
+		}
 
 
-	public void setId_AnnEntrp(int id_AnnEntrp) {
-		Id_AnnEntrp = id_AnnEntrp;
-	}
+		public void setId_AnnEntrp(int id_AnnEntrp) {
+			Id_AnnEntrp = id_AnnEntrp;
+		}
 
 
-	public String getTitre() {
-		return Titre;
-	}
+		public String getTitre() {
+			return Titre;
+		}
 
 
-	public void setTitre(String titre) {
-		Titre = titre;
-	}
+		public void setTitre(String titre) {
+			Titre = titre;
+		}
 
 
-	public String getDescription_Annc() {
-		return Description_Annc;
-	}
+		public String getDomaine() {
+			return Domaine;
+		}
 
 
-	public void setDescription_Annc(String description_Annc) {
-		Description_Annc = description_Annc;
-	}
+		public void setDomaine(String domaine) {
+			Domaine = domaine;
+		}
 
 
-	
-
-	public String getDuree() {
-		return Duree;
-	}
+		public String getDescription_Annc() {
+			return Description_Annc;
+		}
 
 
-	public void setDuree(String duree) {
-		Duree = duree;
-	}
+		public void setDescription_Annc(String description_Annc) {
+			Description_Annc = description_Annc;
+		}
 
 
-	public String getType_Stag() {
-		return Type_Stag;
-	}
+		public String getDuree() {
+			return Duree;
+		}
 
 
-	public void setType_Stag(String type_Stag) {
-		Type_Stag = type_Stag;
-	}
+		public void setDuree(String duree) {
+			Duree = duree;
+		}
 
 
-	public int getId_Entrp() {
-		return Id_Entrp;
-	}
+		public String getType_Stag() {
+			return Type_Stag;
+		}
 
 
-	public void setId_Entrp(int id_Entrp) {
-		Id_Entrp = id_Entrp;
-	}
-	
-	
-	
+		public void setType_Stag(String type_Stag) {
+			Type_Stag = type_Stag;
+		}
 
+
+		public int getId_Entrp() {
+			return Id_Entrp;
+		}
+
+
+		public void setId_Entrp(int id_Entrp) {
+			Id_Entrp = id_Entrp;
+		}
+		
+		
+		
+
+		
+		
+		
+		
 }

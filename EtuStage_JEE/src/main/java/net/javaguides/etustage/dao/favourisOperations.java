27b7931c889773009eb291f99a72a606a9f2c911
Hafsa AIT EL ATIK <hafsaaitelatik@gmail.com>
2023-03-07 14:21:07 +0000

    favourisOperations

---
+++ EtuStage_JEE/src/main/java/net/javaguides/etustage/dao/favourisOperations.java
@@ -0,0 +1,73 @@
+package net.javaguides.etustage.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+import net.javaguides.etustage.model.favouris_entreprise;
+import net.javaguides.etustage.model.favouris_stagaire;
+
+
+public class favourisOperations extends Connect {
+	public ArrayList<favouris_stagaire> getFavourisStagaire() throws SQLException, ClassNotFoundException {
+
+		ArrayList<favouris_stagaire> listOfFavoriteStagaire = new ArrayList<>();
+		PreparedStatement preparedStatement;
+		ResultSet resultSet = null;
+		String sql = "SELECT * from favouris_stagaire JOIN annonce_stagaire WHERE favouris_stagaire.Id_Stag=annonce_stagaire.Id_Stag";
+		preparedStatement = this.connection().prepareStatement(sql);
+		resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			listOfFavoriteStagaire.add(new favouris_stagaire(resultSet.getInt("Id_fav"),
+					resultSet.getInt("Id_Stag"), resultSet.getInt("Id_AnnEntp")));
+					
+		}
+		return listOfFavoriteStagaire;
+	}
+
+	
+	public ArrayList<favouris_entreprise> getFavourisEntreprise() throws SQLException, ClassNotFoundException {
+
+		ArrayList<favouris_entreprise> listOfFavoriteEntreprise = new ArrayList<>();
+		PreparedStatement preparedStatement;
+		ResultSet resultSet = null;
+		String sql = "SELECT * from favouris_entreprise JOIN annonce_entreprise WHERE favouris_entreprise.Id_Entrp=annonce_entreprise.Id_Entrp";
+		preparedStatement = this.connection().prepareStatement(sql);
+		resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			listOfFavoriteEntreprise.add(new favouris_entreprise(resultSet.getInt("Id_fav"),
+					resultSet.getInt("Id_Entrp"), resultSet.getInt("Id_AnnStag")));
+					
+		}
+		return listOfFavoriteEntreprise;
+	}
+	
+	public void deleteAnnonceStag(int id_Stag) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement;
+        ResultSet resultSet=null;
+        String sql="DELETE FROM annonce_stagaire join favouris_stagaire WHERE annonce_stagaire.Id_Stag = favouris_stagaire.Id_stag ;";
+        preparedStatement = this.connection().prepareStatement(sql);
+		preparedStatement.setInt(1, id_Stag);
+		
+
+		resultSet = preparedStatement.executeQuery();
+		
+		
+	}
+	
+	public void deleteAnnonceEntrp(int id_Entrp) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement;
+        ResultSet resultSet=null;
+        String sql="DELETE FROM annonce_entreprise join favouris_entreprise WHERE annonce_entreprise.Id_Entrp = favouris_stagaire.Id_Entrp;";
+        preparedStatement = this.connection().prepareStatement(sql);
+		preparedStatement.setInt(1, id_Entrp);
+
+		resultSet = preparedStatement.executeQuery();
+		
+		
+	}
+	
+}
--- EtuStage_JEE/src/main/java/net/javaguides/etustage/model/annonce_entreprise.java
+++ EtuStage_JEE/src/main/java/net/javaguides/etustage/model/annonce_entreprise.java
@@ -13,8 +13,26 @@ package net.javaguides.etustage.model;
 		private String Duree;
 		private String Type_Stag;
 		private int Id_Entrp;
+		private String St_image ; 
 		
 		
+		
+		
+		
+		public annonce_entreprise(int id_AnnEntrp, String titre, String domaine, String description_Annc, String duree,
+				String type_Stag, int id_Entrp, String st_image) {
+			super();
+			Id_AnnEntrp = id_AnnEntrp;
+			Titre = titre;
+			Domaine = domaine;
+			Description_Annc = description_Annc;
+			Duree = duree;
+			Type_Stag = type_Stag;
+			Id_Entrp = id_Entrp;
+			St_image = st_image;
+		}
+
+
 		public annonce_entreprise(int id_AnnEntrp, String titre, String domaine, String description_Annc, String duree,
 				String type_Stag, int id_Entrp) {
 			super();
@@ -107,6 +125,16 @@ package net.javaguides.etustage.model;
 
 		public void setId_Entrp(int id_Entrp) {
 			Id_Entrp = id_Entrp;
+		}
+
+
+		public String getSt_image() {
+			return St_image;
+		}
+
+
+		public void setSt_image(String st_image) {
+			St_image = st_image;
 		}
 		
 		
